cd8b14c1bc55af939df0d364fd1ca694d284e0bd
Samurais <hailiang.hl.wang@gmail.com>
2014-12-25 15:17:18 +0800

    #20 abstract slides into method

--- app/src/main/java/com/arrking/hyqclient/ui/dashboard/DashIndexActivity.java
+++ app/src/main/java/com/arrking/hyqclient/ui/dashboard/DashIndexActivity.java
@@ -3,11 +3,13 @@ package com.arrking.hyqclient.ui.dashboard;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.*;
+
 import com.arrking.hyqclient.R;
 import com.daimajia.slider.library.Animations.DescriptionAnimation;
 import com.daimajia.slider.library.SliderLayout;
 import com.daimajia.slider.library.SliderTypes.BaseSliderView;
 import com.daimajia.slider.library.SliderTypes.TextSliderView;
+
 import java.util.HashMap;
 
 /**
@@ -16,31 +18,27 @@ import java.util.HashMap;
 public class DashIndexActivity extends Activity implements BaseSliderView.OnSliderClickListener {
 
     private static final String TAG = new String("DashIndexActivity");
-    private SliderLayout mDemoSlider;
+    private SliderLayout recommendSlides;
     // top bar title text
     private TextView tb_title;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.dashboard);
-        tb_title=(TextView)findViewById(R.id.txt_wb_title);
-        tb_title.setText("主页");
 
-        mDemoSlider = (SliderLayout)findViewById(R.id.slider);
-
-//        HashMap<String,String> url_maps = new HashMap<String, String>();
+    // setup recommendations slides
+    private void initRecommendations() {
+        recommendSlides = (SliderLayout) findViewById(R.id.slider);
+        // images can also load from Internet
+        // HashMap<String,String> url_maps = new HashMap<String, String>();
 //        url_maps.put("Hannibal", "http://static2.hypable.com/wp-content/uploads/2013/12/hannibal-season-2-release-date.jpg");
 //        url_maps.put("Big Bang Theory", "http://tvfiles.alphacoders.com/100/hdclearart-10.png");
 //        url_maps.put("House of Cards", "http://cdn3.nflximg.net/images/3093/2043093.jpg");
 //        url_maps.put("Game of Thrones", "http://images.boomsbeat.com/data/images/full/19640/game-of-thrones-season-4-jpg.jpg");
 
-        HashMap<String,Integer> file_maps = new HashMap<String, Integer>();
-        file_maps.put("女人节到哪里?",R.drawable.demo_dummy_activity1);
-        file_maps.put("新品推荐",R.drawable.demo_dummy_activity2);
-        file_maps.put("香辣鸡腿",R.drawable.demo_dummy_activity3);
+        HashMap<String, Integer> file_maps = new HashMap<String, Integer>();
+        file_maps.put("女人节到哪里?", R.drawable.demo_dummy_activity1);
+        file_maps.put("新品推荐", R.drawable.demo_dummy_activity2);
+        file_maps.put("香辣鸡腿", R.drawable.demo_dummy_activity3);
 
-        for(String name : file_maps.keySet()){
+        for (String name : file_maps.keySet()) {
             TextSliderView textSliderView = new TextSliderView(this);
             // initialize a SliderLayout
             textSliderView
@@ -51,20 +49,33 @@ public class DashIndexActivity extends Activity implements BaseSliderView.OnSlid
 
             //add your extra information
             textSliderView.getBundle()
-                    .putString("extra",name);
+                    .putString("extra", name);
 
-            mDemoSlider.addSlider(textSliderView);
+            recommendSlides.addSlider(textSliderView);
         }
-        mDemoSlider.setPresetTransformer(SliderLayout.Transformer.Accordion);
-        mDemoSlider.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
-        mDemoSlider.setCustomAnimation(new DescriptionAnimation());
-        mDemoSlider.setDuration(4000);
+        recommendSlides.setPresetTransformer(SliderLayout.Transformer.Accordion);
+        recommendSlides.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
+        recommendSlides.setCustomAnimation(new DescriptionAnimation());
+        recommendSlides.setDuration(4000);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard);
+        // set title
+        tb_title = (TextView) findViewById(R.id.txt_wb_title);
+        tb_title.setText("主页");
+
+        // init recommendations
+        initRecommendations();
+
 //        ListView l = (ListView)findViewById(R.id.transformers);
 //        l.setAdapter(new TransformerAdapter(this));
 //        l.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 //            @Override
 //            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                mDemoSlider.setPresetTransformer(((TextView) view).getText().toString());
+//                recommendSlides.setPresetTransformer(((TextView) view).getText().toString());
 //                Toast.makeText(DashIndexActivity.this, ((TextView) view).getText().toString(), Toast.LENGTH_SHORT).show();
 //            }
 //        });
@@ -72,7 +83,7 @@ public class DashIndexActivity extends Activity implements BaseSliderView.OnSlid
 
     @Override
     public void onSliderClick(BaseSliderView slider) {
-        Toast.makeText(this, slider.getBundle().get("extra") + "",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, slider.getBundle().get("extra") + "", Toast.LENGTH_SHORT).show();
     }
 
 }
