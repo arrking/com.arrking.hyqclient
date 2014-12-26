ebd0eb70396278de52fca68339c97c8d865172bd
Samurais <hailiang.hl.wang@gmail.com>
2014-12-26 20:32:20 +0800

    #21 init UI but haven't load and display data yet

--- app/src/main/java/com/arrking/hyqclient/ui/contacts/ContxIndexActivity.java
+++ app/src/main/java/com/arrking/hyqclient/ui/contacts/ContxIndexActivity.java
@@ -3,6 +3,11 @@ package com.arrking.hyqclient.ui.contacts;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.arrking.hyqclient.R;
@@ -10,17 +15,55 @@ import com.arrking.hyqclient.R;
 /**
  * Created by hain on 23/12/2014.
  */
-public class ContxIndexActivity extends Activity {
+public class ContxIndexActivity extends Activity implements  AdapterView.OnItemClickListener {
 
     private static final String TAG = new String("ContxIndexActivity");
     // top bar title text
     private TextView tb_title;
+    private ListView peopleListView;
+    private QuickAlphabeticBar alpha;
+    private EditText searchBoxEditText;
+
+
+    View.OnTouchListener touchListener = new View.OnTouchListener()
+    {
+        public boolean onTouch(View paramAnonymousView, MotionEvent paramAnonymousMotionEvent)
+        {
+            // do your ontouch magic
+            // hide keyboard, etc ...
+            return false;
+        }
+    };
 
     public void onCreate(Bundle paramBundle) {
         super.onCreate(paramBundle);
-        Log.d(TAG, "get run .... ");
         setContentView(R.layout.contacts);
-        tb_title = (TextView) findViewById(R.id.txt_wb_title);
-        tb_title.setText("通讯录");
+        initUI();
+
+    }
+
+    private void initUI() {
+        this.tb_title = (TextView) findViewById(R.id.txt_wb_title);
+        this.tb_title.setText("通讯录");
+        this.peopleListView = (ListView) findViewById(R.id.maillist_layout_main_listView);
+        this.peopleListView.setOnTouchListener(this.touchListener);
+        this.peopleListView.setOnItemClickListener(this);
+        this.peopleListView.setDivider(getResources().getDrawable(R.drawable.line_image));
+        this.alpha = ((QuickAlphabeticBar)findViewById(R.id.fast_scroller_view));
+        this.searchBoxEditText = (EditText) findViewById(R.id.edit);
+        int i = View.MeasureSpec.makeMeasureSpec(0, 0);
+        int j = View.MeasureSpec.makeMeasureSpec(0, 0);
+        this.searchBoxEditText.measure(i,j);
+//        Constants.TOPSEARCH = this.editText.getMeasuredHeight();
+        loadAndDisplayContacts();
+    }
+
+    private void loadAndDisplayContacts() {
+        
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        // do open contact card
     }
 }
